2589e4b41c80ed56d65a8cf2ddc93db1ebdff0cf
Jaqueline <juanajaquelinecamarillo@gmail.com>
2025-02-21 08:50:05 -0600

    Jacky

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,12 +3,11 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { GameFormComponent } from './components/game-form/game-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GameFormComponent
+    
   ],
   imports: [
     BrowserModule,
