4b7a5ff4414b8a14ed3a7b63f30c092eb03aee85
roygro <princess18gro@gmail.com>
2025-03-10 15:44:15 -0600

    ordenantes-rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,9 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { GestionOperadorComponent } from './components/gestion-operador/gestion-operador.component';
-
+import { CommonModule } from '@angular/common';
+import { GestionOrdenantesComponent } from './components/ordenantes/gestion-ordenantes/gestion-ordenantes.component';
+import { ReactiveFormsModule } from '@angular/forms';  // Importa ReactiveFormsModule
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
@@ -15,6 +17,7 @@ import { PrincipalAdminComponent } from './components/administrador/principal-ad
 import { GestionAdminComponent } from './components/administrador/gestion-admin/gestion-admin.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { DepositoCuentaComponent } from './components/ordenantes/deposito-cuenta/deposito-cuenta.component';
 
 
 @NgModule({
@@ -24,6 +27,9 @@ import { MatIconModule } from '@angular/material/icon';
     DashboardComponent,
     PrincipalAdminComponent,
     GestionAdminComponent,
+    GestionOrdenantesComponent,
+    DepositoCuentaComponent
+
 
   ],
   imports: [
@@ -34,7 +40,9 @@ import { MatIconModule } from '@angular/material/icon';
     MatButtonModule,
     MatInputModule,
     MatCardModule,
-    MatIconModule
+    MatIconModule,
+    CommonModule,
+    ReactiveFormsModule,
   ],
   providers: [
     provideClientHydration()
